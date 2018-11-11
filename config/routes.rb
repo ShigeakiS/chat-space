8228f617d1b4227f6910a225162e2859b0daeff6
ShigeakiS <shigeaki@shigeakinoMacBook-puro.local>
2018-11-11 21:23:34 +0900

    add-groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'messages' => 'messages#index'
-  root 'messages#index'
+  root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
 end
